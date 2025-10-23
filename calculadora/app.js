2ebb413e998b372570664abadedd21bf2cba8698
Eric Reullyson <141361309+Reullyson@users.noreply.github.com>
2025-10-23 16:42:47 -0300

    Refactor variable names for consistency

--- calculadora/app.js
+++ calculadora/app.js
@@ -1,9 +1,9 @@
-let currentInput = document.querySelector('.currentInput');
-let answerScreen = document.querySelector('.answerScreen');
-let buttons = document.querySelectorAll('button');
-let erasebtn = document.querySelector('#erase');
-let clearbtn = document.querySelector('#clear');
-let evaluate = document.querySelector('#evaluate');
+const currentInput = document.querySelector('.currentInput');
+const answerScreen = document.querySelector('.answerScreen');
+const buttons = document.querySelectorAll('button');
+const eraseBtn = document.querySelector('#erase');
+const clearBtn = document.querySelector('#clear');
+const evaluateBtn = document.querySelector('#evaluate');
 
 let realTimeScreenValue = [];
 
@@ -17,16 +17,16 @@ function calculateExpression(expr) {
   }
 }
 
-clearbtn.addEventListener("click", () => {
+clearBtn.addEventListener('click', () => {
   realTimeScreenValue = [''];
   answerScreen.innerHTML = 0;
   currentInput.className = 'currentInput';
   answerScreen.className = 'answerScreen';
-  answerScreen.style.color = "rgba(150, 150, 150, 0.87)";
+  answerScreen.style.color = 'rgba(150, 150, 150, 0.87)';
 });
 
 buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener('click', () => {
     if (!btn.id.match('erase') && !btn.id.match('evaluate') && !btn.id.match('clear')) {
       realTimeScreenValue.push(btn.value);
       currentInput.innerHTML = realTimeScreenValue.join('');
@@ -44,7 +44,7 @@ buttons.forEach((btn) => {
     if (btn.id.match('evaluate')) {
       currentInput.className = 'answerScreen';
       answerScreen.className = 'currentInput';
-      answerScreen.style.color = "white";
+      answerScreen.style.color = 'white';
     }
   });
 });
